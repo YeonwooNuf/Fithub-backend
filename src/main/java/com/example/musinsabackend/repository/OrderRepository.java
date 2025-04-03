15e82b8165f6f8342bfd83fae579b62ac1ebd215
장연우 <jangyw0927@naver.com>
2025-04-03 17:43:25 +0900

    // 결제 ID 중복 확인

--- src/main/java/com/example/musinsabackend/repository/OrderRepository.java
+++ src/main/java/com/example/musinsabackend/repository/OrderRepository.java
@@ -3,8 +3,8 @@ package com.example.musinsabackend.repository;
 import com.example.musinsabackend.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUser_Username(String username);
+
+    // ✅ 결제 ID 중복 여부 확인
+    boolean existsByPaymentId(String paymentId);
 }
