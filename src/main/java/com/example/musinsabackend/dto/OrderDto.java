398a3d2fcad1b63a538acae965ca260dbf8198b9
장연우 <jangyw0927@naver.com>
2025-04-04 10:20:03 +0900

    // Getter & Setter 롬복

--- src/main/java/com/example/musinsabackend/dto/OrderDto.java
+++ src/main/java/com/example/musinsabackend/dto/OrderDto.java
@@ -1,58 +1,17 @@
 package com.example.musinsabackend.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
 public class OrderDto {
-    private Long id;
-    private Long productId; // 상품 ID 필드 추가
-    private String productName;
-    private int quantity;
+    private Long orderId;
+    private String paymentId;
+    private int totalAmount;
+    private int finalAmount;
+    private int usedPoints;
     private String orderDate;
-    private String status;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getProductId() { // 상품 ID getter
-        return productId;
-    }
-
-    public void setProductId(Long productId) { // 상품 ID setter
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(String orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    private List<OrderItemDto> items; // ✅ 외부 DTO로 분리
 }
