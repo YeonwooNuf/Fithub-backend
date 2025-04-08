aa0d480e81863a4e8eda3acfdc08fbb4d2d0d9be
장연우 <jangyw0927@naver.com>
2025-04-08 16:13:48 +0900

    // 주문 사용 쿠폰 검색 메소드 추가

--- src/main/java/com/example/musinsabackend/repository/OrderRepository.java
+++ src/main/java/com/example/musinsabackend/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.example.musinsabackend.repository;
 
 import com.example.musinsabackend.model.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
@@ -12,4 +16,10 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     // ✅ 사용자 ID로 주문 내역 조회
     List<Order> findByUser_UserId(Long userId);
+
+    @EntityGraph(attributePaths = "usedCoupons")
+    Optional<Order> findWithUsedCouponsById(Long id);
+
+    @Query("SELECT o FROM Order o LEFT JOIN FETCH o.usedCoupons WHERE o.id = :orderId")
+    Optional<Order> findWithCouponsById(@Param("orderId") Long orderId);
 }
