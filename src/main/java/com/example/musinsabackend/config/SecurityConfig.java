6acf1bb0aab8207029bedb744f1dda05d852566a
장연우 <jangyw0927@naver.com>
2025-04-15 14:58:47 +0900

    // 카카오 로그인 경로 접근 허용

--- src/main/java/com/example/musinsabackend/config/SecurityConfig.java
+++ src/main/java/com/example/musinsabackend/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/users/login", "/api/users/register").permitAll() // ✅ 인증 없이 허용
-                        .requestMatchers(HttpMethod.GET, "/uploads/**","/api/users/home","/api/products","/api/products/**", "/api/community/posts").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/uploads/**","/api/users/home","/api/products","/api/products/**",
+                                "/api/oauth/kakao/callback","/api/community/posts").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/events/**", "/api/users/mypage", "/api/cart/**","/api/payment/**").authenticated()
                         .requestMatchers(HttpMethod.GET, "/api/users/addresses").authenticated()
                         .requestMatchers("/api/likes/**").authenticated()
