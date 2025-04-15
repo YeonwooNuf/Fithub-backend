37111fbdc8114c810a61fcc8fe84ce9f9a179285
장연우 <jangyw0927@naver.com>
2025-04-15 15:57:53 +0900

    // 가입 방식 받는 메소드

--- src/main/java/com/example/musinsabackend/repository/UserRepository.java
+++ src/main/java/com/example/musinsabackend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.musinsabackend.repository;
 
 import com.example.musinsabackend.model.user.User;
+import com.example.musinsabackend.model.user.AuthProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByNickname(String nickname);
+
+    Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
 }
